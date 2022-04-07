1f7db0f1334d71e01b6e74c220b3b951d48d39c0
zerodice0 <zerodice0@gmail.com>
2022-04-08 00:58:58 +0900

    feat: display loading... while http communication by API

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,6 @@
-import styled from "styled-components";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Title = styled.h1`
   font-size: 48px;
@@ -19,6 +20,17 @@ const Header = styled.header`
   align-items: center;
 `;
 
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Image = styled.img`
+  width: 25px;
+  height: 25px;
+  margin-right: 10px;
+`;
+
 const CoinsList = styled.ul``;
 
 const Coin = styled.li`
@@ -29,6 +41,13 @@ const Coin = styled.li`
   border-radius: 15px;
   transition: color 1s ease-in;
 
+  a {
+    display: flex;
+    align-itmes: center;
+    padding: 20px;
+    transition: color 0.2s ease-in;
+  }
+
   &:hover {
     a {
       color: ${props => props.theme.accentColor};
@@ -37,49 +56,57 @@ const Coin = styled.li`
   }
 `;
 
-const coins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-];
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 const Coins = () => {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const data = await response.json();
+      setCoins(data.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <Container>
       <Header>
         <Title>Coins</Title>
       </Header>
-      <CoinsList>
-        {coins.map(coin => (
-          <Coin key={coin.id}>
-            <Link to={`/:${coin.id}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinsList>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map(coin => (
+            <Coin key={coin.id}>
+              <Link
+                to={`/:${coin.id}`}
+                state={{
+                  name: coin.name,
+                  id: coin.id,
+                }}
+              >
+                <Image
+                  alt={coin.name}
+                  src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
     </Container>
   );
 };
