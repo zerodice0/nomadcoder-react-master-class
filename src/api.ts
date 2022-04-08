7a16a443907d66a18e7733ea9784d18c394f5f78
zerodice0 <zerodice0@gmail.com>
2022-04-09 02:07:04 +0900

    feat: add fetchCoinHistory api

--- src/api.ts
+++ src/api.ts
@@ -12,3 +12,12 @@ export const fetchCoinTickers = async (coinId: string) =>
   fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}/`).then(response =>
     response.json()
   );
+
+export const fetchCoinHistory = async (coinId: string) => {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 30;
+
+  return fetch(
+    `https://api.coinpaprika.com/v1/coins/${coinId}/ohlcv/historical?start=%{startDate}&end=%{endDate}`
+  ).then(response => response.json());
+};
