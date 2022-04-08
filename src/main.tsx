c60554f91a62d2f15a38b2ea4776c3c869df3e5b
zerodice0 <zerodice0@gmail.com>
2022-04-09 03:54:52 +0900

    fix: move to ThemeProvider to App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,15 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ThemeProvider } from "styled-components";
 
 import App from "./App";
-import { theme } from "./theme";
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <App />
     </QueryClientProvider>
   </StrictMode>,
   document.getElementById("root")
