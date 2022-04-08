87ae31490da2edc8f9140e2aec6520f0328e1653
zerodice0 <zerodice0@gmail.com>
2022-04-09 03:54:06 +0900

    feat: distribute darkTheme and lightTheme - add boxColor - change textColor to black and white tone

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,15 @@
 import { DefaultTheme } from "styled-components";
 
-export const theme: DefaultTheme = {
-  textColor: "#c8d6e5",
+export const darkTheme: DefaultTheme = {
+  textColor: "#2f3136",
   backgroundColor: "#222f3e",
   accentColor: "#1dd1a1",
+  boxColor: "white",
+};
+
+export const lightTheme: DefaultTheme = {
+  textColor: "white",
+  backgroundColor: "white",
+  accentColor: "#1dd1a1",
+  boxColor: "#2f3136",
 };
